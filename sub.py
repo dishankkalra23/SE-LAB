65629671b07b6cf359d23b4fcbcfd53f172375ea
mui-pks <pranaykumar1604@gmail.com>
2020-08-14 11:38:14 +0530

    Subtraction commited

---
+++ sub.py
@@ -0,0 +1,6 @@
+def subtraction(x,y):  #function definifion for subtraction
+    sub=x-y
+    return sub
+num1=int(input("please enter first number: "))#input from user to num1
+num2=int(input("please enter second number: "))#input from user to num2
+print("Subtraction is: ",subtraction(num1,num2))#call the function
